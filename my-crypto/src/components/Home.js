6dc2a0100acd0704abf5e73d7b698520b923cd20
Haroon-1ktk <121959975+Haroon-1ktk@users.noreply.github.com>
2023-08-03 08:09:28 -0700

    update Home.js

--- my-crypto/src/components/Home.js
+++ my-crypto/src/components/Home.js
@@ -1,17 +1,30 @@
 import React from 'react'
+import Info from './Info'
 
 const Home = () => {
   return (
     <>
-    <div className='bg-gradient-to-l from-[#0D324D] to-[#7F5A83] h-[100%] w-100%flex'>
-      <div className=''>
-        <img src='https://plus.unsplash.com/premium_photo-1675883162003-dce379a05e72?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=388&q=80'
-         alt='bitcoin' className='w-[50%] h-[561px]'/>
+      <div className='bg-gradient-to-l from-[#0D324D] to-[#7F5A83] h-full w-100% flex md:flex-row flex-col justify-center'>
+        <div className='md:w-[50%] md:m-14 mt-2 w-[400px] '>
+          <img src='https://plus.unsplash.com/premium_photo-1675883162003-dce379a05e72?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=388&q=80'
+            alt='bitcoin' className='md:w-[400px]   md:h-[400px] w-[450px] md:ml-7  rounded-lg' />
+        </div>
+        <div className='w-[50%]'>
+          <h1 className='font-bold text-white text-6xl pt-14 '>
+            Progress<br/>
+            Your Bitcoin Money Success</h1>
+            <p className='font-bold p-3 text-white'>There are many variations of passages of Lorem Ipsum available,<br/>
+             but the majority have suffered</p>
+             <div className='mt-6 '>
+              <button className='px-6 py-2 ml-2 mb-1 text-white font-semibold hover:bg-[#7F5A83] border-white border-2
+              rounded-lg'>Read More</button>
+              <button className='px-6 py-2 text-white font-semibold hover:bg-[#7F5A83] border-white border-2
+             md:ml-4 ml-2 rounded-lg'>Get Quote</button>
+             </div>
+        </div>
       </div>
-      <div className='w-full'>
-
-      </div>
-      </div></>
+      <Info/>
+      </>
   )
 }
 
---
+++ my-crypto/src/components/Info.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const information=[{
+    title:"Online Wallet",
+    paragraph:"it is a longestablished fact that a readerwill be distracted by",
+},{
+title:"Send Coins",
+    paragraph:"it is a longestablished fact that a readerwill be distracted by",
+},{
+title:"Coin Minnig",
+    paragraph:"it is a longestablished fact that a readerwill be distracted by",
+},{
+    title:"Coin Minnig",
+    paragraph:"it is a longestablished fact that a readerwill be distracted by",
+}   
+]
+
+
+const Info = () => {
+  return (
+   <>
+   <section className='bg-gradient-to-l from-[#0D324D] to-[#7F5A83] grid md:grid-cols-4 grid-cols-1 '>
+    {information.map((i)=>(
+        <div className='mt-14 mx-4 ml-4 bg-white rounded-xl shadow-md hover:bg-yellow-500
+        hover:ease-in duration-300 hover:transform hover:text-white h-[240px]'>
+            <div className='mt-3 mb-3 ml-16'><h1 className='font-bold md:text-2xl text-md'>{i.title}</h1></div>
+            <div  className='mt-3 mb-3 ml-10 '>
+                <p className='md:font-semibold text-gray-400 md:text-base text-xs hover:text-white'>{i.paragraph}</p>
+                </div>
+        </div>
+    ))}
+   </section>
+   </>
+  )
+}
+
+export default Info
