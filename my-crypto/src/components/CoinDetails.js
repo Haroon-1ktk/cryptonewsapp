0c5d02d576f87241fce7f6bc68db2c93a833db20
Haroon-1ktk <121959975+Haroon-1ktk@users.noreply.github.com>
2023-08-05 09:43:23 -0700

    updatecoindetails

--- my-crypto/src/components/CoinDetails.js
+++ my-crypto/src/components/CoinDetails.js
@@ -1,11 +1,21 @@
 import React,{useState,useEffect} from 'react'
-
-const CoinDetails = () => {
+import { useParams } from 'react-router-dom';
+import axios from 'axios'
+const CoinDetails = ({id}) => {
+  const params=useParams()
   const [loading,setLoading]=useState(true);
   const [currency,setCurrency]=useState("pkr")
+  const [coin,setCoin]=useState({})
   const Hello=(e)=>{
     setCurrency(e.target.value)
   }
+  useEffect(()=>{
+    const fetchme=async()=>{
+      const {data}=await axios.get(`https://api.coingecko.com/api/v3/coins/${params.id}`)
+   console.log(data.tickers)
+      setLoading(false)}
+      fetchme()
+  },[params.id])
   return (
     <>
     <foam className='mt-3 ml-[550px]' onChange={Hello}>
@@ -13,6 +23,7 @@ const CoinDetails = () => {
          <input type='radio' value="Eur" name="currency"/>Eur 
          <input type='radio' value="Usd" name="currency" />Usd 
   </foam>
+  {coin.coin_id}
     </>
   )
 }
